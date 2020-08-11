1ac8c891340ea7b2554d6157ab5999327c4875bf
wangweihua <wangweihua@vipkid.com.cn>
2020-08-11 20:26:42 +0800

    feat: update ch02 with Color

--- src/ch02.points/index.ts
+++ src/ch02.points/index.ts
@@ -1,4 +1,4 @@
-import { OOWebGL } from '../../oowebgl/dist/oowebgl';
+import { OOWebGL, Color } from '../../oowebgl/dist/oowebgl';
 (async () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const owgl = new OOWebGL(canvas);
@@ -11,10 +11,10 @@ import { OOWebGL } from '../../oowebgl/dist/oowebgl';
   const prog = window.g = await owgl.createProgram();
   await prog.attachShaders(...shaders).linkAndUse()
   owgl.clear();
-  prog.setUniform('u_FragColor', 1, 0, 0, 1);
+  prog.setUniform('u_FragColor', ...Color.MAGENTA.vec4);
   prog.setAttribute('a_Position', -0.5, 0, 0);
   owgl.draw('POINTS', 1);
   prog.setAttribute('a_Position', 0.5, 0, 0.0);
-  prog.setUniform('u_FragColor', 1, 1, 0, 1);
+  prog.setUniform('u_FragColor', ...Color.from(1, 1, 1, 0.5).vec4);
   owgl.draw('POINTS', 1);
 })();
